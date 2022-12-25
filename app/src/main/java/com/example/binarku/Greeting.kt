186ed2365976802f77a4ffdce6809959048b8f28
unknown <xh.abraham@gmail.com>
2022-12-25 17:49:39 +0700

    batuguntingkertas

---
+++ app/src/main/java/com/example/binarku/Greeting.kt
@@ -0,0 +1,9 @@
+package com.example.binarku
+
+class Greeting {
+    fun welcome(){
+        println("==========================")
+        println("GAME SUIT TERMINAL VERSION")
+        println("==========================")
+    }
+}
